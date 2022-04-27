9e71db6b176fb59b3723fc3e2496a866f4cc93d0
Muryginds <muryginds@gmail.com>
2022-04-27 14:07:05 +0300

    Logging added, packages added, classes renamed

--- src/main/java/SearchEngine.java
+++
@@ -1,6 +0,0 @@
-public class SearchEngine {
-
-  public static void main(String[] args) {
-
-  }
-}
---
+++ src/main/java/com/muryginds/searchEngine/model/Page.java
@@ -0,0 +1,37 @@
+package com.muryginds.searchEngine.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "pages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "path")
+    String path;
+
+    @Column(name = "code")
+    Integer code;
+
+    @Column(name = "content")
+    String content;
+}
