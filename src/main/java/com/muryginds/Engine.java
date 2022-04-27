a4ddcc100d7553188afe8bc57c84dd467939b78b
Muryginds <muryginds@gmail.com>
2022-04-27 15:02:26 +0300

    Log configuration and gitignore modified

--- src/main/java/com/muryginds/Engine.java
+++ src/main/java/com/muryginds/Engine.java
@@ -1,14 +1,16 @@
 package com.muryginds;
 
 import com.muryginds.searchEngine.parser.SiteParser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Engine {
 
   private static final String URL = "https://tatparts.ru";
 
   public static void main(String[] args) {
-    //SpringApplication.run(Engine.class, args);
+    SpringApplication.run(Engine.class, args);
     new SiteParser().parse(URL);
   }
 }
