b7cffa8f763691d748aa67d25602a223922d3dcd
Muryginds <muryginds@gmail.com>
2022-04-29 12:10:23 +0300

    Morphology library, LemmaConverter added

---
+++ src/main/java/com/muryginds/searchEngine/morthology/Morphology.java
@@ -0,0 +1,12 @@
+package com.muryginds.searchEngine.morthology;
+
+import java.io.IOException;
+
+public class Morphology {
+
+  public static void main(String[] args) throws IOException {
+  String string = "Повторное появление леопарда в Осетии позволяет предположить,"
+      + " что леопард постоянно обитает в некоторых районах Северного Кавказа.";
+  System.out.println(new LemmaConverter().convert(string));
+  }
+}
