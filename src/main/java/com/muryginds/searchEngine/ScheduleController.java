abc3aa4cface157407610d13246772d1c6c0582f
Dmitry Murygin <muryginds@gmail.com>
2022-05-12 10:36:00 +0300

    Dev (#12)
    * PageIndexer logging added
    
    * Morphology.class removed
    
    * SiteProcessingService added, minor changes

--- src/main/java/com/muryginds/searchEngine/ScheduleController.java
+++ src/main/java/com/muryginds/searchEngine/ScheduleController.java
@@ -1,7 +1,6 @@
 package com.muryginds.searchEngine;
 
-import com.muryginds.searchEngine.indexer.PageIndexer;
-import com.muryginds.searchEngine.parser.LinkParser;
+import com.muryginds.searchEngine.service.SiteProcessingService;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +15,13 @@ import org.springframework.stereotype.Component;
 public class ScheduleController {
   @Setter
   private List<Map<String, String>> sites;
-  private final LinkParser linkParser;
-  private final PageIndexer pageIndexer;
+  private final SiteProcessingService siteProcessingService;
 
   @Scheduled(fixedDelayString = "P1D")
   public void worker() {
-    siteParser();
-    siteIndexer();
-  }
-
-  public void siteParser() {
     for (Map<String, String> siteParams : sites) {
-      linkParser.parse(siteParams.get("url"), siteParams.get("name"));
+      siteProcessingService.process(siteParams.get("url"), siteParams.get("name"));
       break;
     }
-  }
-
-  private void siteIndexer() {
-    pageIndexer.index();
   }
 }
